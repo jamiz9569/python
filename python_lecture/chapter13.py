c33a342d0db44db94666f02367b00ac08fdf889c
jamiz qamar <jamizalam1@gmail.com>
2026-06-22 23:55:51 +0530

    multi threading

---
+++ python_lecture/chapter13.py
@@ -0,0 +1,27 @@
+# multi threading in python 
+import threading
+import time
+
+def print_numbers():
+    for i in range(5):
+        print(i)
+        time.sleep(1)
+
+def print_letters():
+    for letter in 'abcde':
+        print(letter)
+        time.sleep(1)
+
+# Create threads
+t1 = threading.Thread(target=print_numbers)
+t2 = threading.Thread(target=print_letters)
+
+# Start threads
+t1.start()
+t2.start()
+
+# Wait for threads to complete
+t1.join()
+t2.join()
+print("Done!")
+
